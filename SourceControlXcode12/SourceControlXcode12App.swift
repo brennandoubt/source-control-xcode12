3a26689d58495e594c67d3d84dd380982f2683af
Brennan Allen <brennandoubt@gmail.com>
2023-02-03 19:26:57 -0500

    Initial Commit

---
+++ SourceControlXcode12/SourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12App.swift
+//  SourceControlXcode12
+//
+//  Created by Brennan Allen on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
